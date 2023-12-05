7a8d2ba0788866fb5f1819800815ed6e78158076
Myat Thiha <myat@infinitalk.co.jp>
2023-12-06 04:40:23 +0630

    Update LoadingSpinner.tsx

--- frontend/src/components/LoadingSpinner/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,8 +1,8 @@
 // react
 import React from 'react';
 // mui
-import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 const LoadingSpinner: React.FC = () => {
   return (
