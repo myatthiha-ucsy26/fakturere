3ea17994f688de5331ee2cfbd15fb3e03697901d
Myat Thiha <myat@infinitalk.co.jp>
2023-12-05 12:49:03 +0630

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  base: './',
-  build: {
-    target: 'es2015',
-  },
   plugins: [
     legacy({
       targets: ['chrome >= 64'], // expected compatible browser target range
