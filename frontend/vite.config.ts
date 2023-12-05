cb76d0857f176847ca3f5555dd8d00f1e89bfd6d
Myat Thiha <myat@infinitalk.co.jp>
2023-12-05 12:33:57 +0630

    fakturere

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    target: 'chrome70',
-  }
+  base: './',
+  build: {
+    target: 'es2015',
+  },
+  plugins: [
+    legacy({
+      targets: ['chrome >= 64'], // expected compatible browser target range
+      renderLegacyChunks: true, // need to generate legacy browser compatible chunks (default true)
+      modernPolyfills: true, // no need to generate polyfills block for modern browsers (default false)
+    }),
+  ],
 });
