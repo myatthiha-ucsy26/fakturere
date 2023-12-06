4ce4b36d3172d2e62f86c8ecf6fb3750fdc3b334
Myat Thiha <myat@infinitalk.co.jp>
2023-12-06 17:27:24 +0630

    fakturere

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,5 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [
-    legacy({
-      targets: ['chrome >= 64'],
-      renderLegacyChunks: true,
-      modernPolyfills: true,
-    }),
-  ],
 });
